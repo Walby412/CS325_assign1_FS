b6304f837366ab9653579a1b73645f8b30d2052c
Fabian Spanring <102763909+Walby412@users.noreply.github.com>
2024-09-09 15:27:19 -0500

    Update program1.py
    github change

--- program1.py
+++ program1.py
@@ -1,5 +1,5 @@
 def numbers(a, b):
     print("The multiplication of the two numbers are: ", a*b)
-    print("The sum of the two numbers is: ", a+b)
+    print("The subtraction of the two numbers is: ", a+b)
 
 numbers(2, 3)
